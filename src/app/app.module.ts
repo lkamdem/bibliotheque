5991a8365b41bfe4680f3e05a45ce2b03d19dfd5
diablo102 <kamdemleo@yahoo.fr>
2021-10-28 17:24:33 +0200

    header template implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes:Routes = [
   { path: 'auth/signup', component:SignupComponent },
-  { path: 'auth/signIn', component:SigninComponent },
+  { path: 'auth/signin', component:SigninComponent },
   { path: 'books', component:BookListComponent },
   { path: 'books/new', component:BookFormComponent},
   { path: 'books/view/:id', component:SingleBookComponent }
