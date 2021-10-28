aa89e9ffbc1dee2fe87d1c2513bbc8cd2a3715ea
diablo102 <kamdemleo@yahoo.fr>
2021-10-28 17:44:10 +0200

    add firebase to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { initializeApp } from "firebase/app";
+//import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bibliotheque';
+
+  constructor() {
+    const firebaseConfig = {
+
+      apiKey: "AIzaSyCVhGUcNjv3lvpwAPVK_NfsXh3DFU1z-64",
+    
+      authDomain: "bibliotheque-b9af9.firebaseapp.com",
+    
+      projectId: "bibliotheque-b9af9",
+    
+      storageBucket: "bibliotheque-b9af9.appspot.com",
+    
+      messagingSenderId: "375753627825",
+    
+      appId: "1:375753627825:web:b2aae443ac8bde65772a90",
+    
+      measurementId: "G-8L5LY91GKY"
+    
+    };
+
+    //firebase.initializeApp(firebaseConfig)
+    const app = initializeApp(firebaseConfig);
+
+
+    
+  }
 }
