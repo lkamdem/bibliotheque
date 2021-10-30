c594f7c8027dcfa4f5db996b0168206ac3637017
diablo102 <kamdemleo@yahoo.fr>
2021-10-30 15:15:24 +0200

    reactiveForm for signUp component

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+//import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import * as firebase from 'firebase/auth'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  auth = firebase.getAuth();
+
   constructor() { }
+  
+
+  createNewUser(email:string, password:string) {
+    return new Promise<void>(
+      (resolve,reject) => {
+        firebase.createUserWithEmailAndPassword(this.auth, email, password).then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  signInUser(email:string, password:string) {
+    return new Promise<void>(
+      (resolve, reject) => {
+        firebase.signInWithEmailAndPassword(this.auth, email, password).then(
+         () => {
+           resolve();
+         },
+         (error) => {
+           reject(error);
+         }
+        );
+        }
+    );
+  }
+
+  signOutUser() {
+//    firebase.signOut(this.auth)
+    firebase.getAuth().signOut();
+  }
+
 }
