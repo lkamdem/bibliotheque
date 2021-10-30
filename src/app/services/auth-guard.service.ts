e9d94edbe002eca81eca7996f24f9df8167dfe3d
diablo102 <kamdemleo@yahoo.fr>
2021-10-30 20:54:00 +0200

    set guard on routes

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { FirebaseError } from '@firebase/util';
+import { Observable } from 'rxjs';
+import * as firebase from 'firebase/auth'
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private router:Router) { }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.onAuthStateChanged(firebase.getAuth(),
+          (user) => {
+            if (user) {
+              resolve(true);
+            } else {
+              this.router.navigate(['/auth', 'signin']);
+              resolve(false);
+            }
+          }
+        );
+      }
+    );
+  }
 }
